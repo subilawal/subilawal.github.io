78876950d7ac6b528b32ba309d1a2288486a1431
subilawal <subil0214@gmail.com>
2026-07-27 17:41:08 -0400

    fix vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import About from './components/About'
 import Contact from './components/Contact'
-import Experience from './components/Experience'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 
